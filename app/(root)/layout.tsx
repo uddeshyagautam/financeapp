60518e82080db277a34c58e3df0d7eb98aa16e2b
uddeshyagautam <uddeshyagautam8@gmail.com>
2024-10-14 06:00:20 -0700

    implement the complete layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,31 @@
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+import MobileNav from "@/components/ui/MobileNav";
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = {
+    firstName: "Uddeshya",
+    lastName: "JSM",
+    // Replace with actual user data when implementing login/logout functionality
+    // Example: const loggedIn = JSON.parse(localStorage.getItem('user')) || {};  // Assuming user data is stored in local storage.
+    // Note: This is a simplified example, and you may need to adapt it to your specific use case.
+  };
+
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className="flex h-screen  w-full font-inter">
+      <Sidebar user={loggedIn} />
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="icon" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/ui/TotalBalanceBox";
+import RightSidebar from "@/components/ui/RightSidebar";
 
 const Home = () => {
-  return <div> Home</div>;
+  const loggedIn = {
+    firstName: "Uddeshya",
+    lastName: "JSM",
+    email: "contact@uddeshya.pro",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500.5 }]}
+      />
+    </section>
+  );
 };
 
 export default Home;
